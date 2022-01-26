ce54c9dc567c7937ed52b44d658a637686b2e15d
Serega Kordeluk <serega.wallace@gmail.com>
2022-01-26 13:39:41 +0300

    app-info

---
+++ src/components/app-info/app-info.js
@@ -0,0 +1,11 @@
+
+
+const AppInfo = () => {
+    return (
+        <div className="app-info">
+            <h1>Accounting For Employees In The Company</h1>
+            <h2>Total Number Of Employees: </h2>
+            <h2>Employees Of The Month: </h2>
+        </div>
+    )
+}
