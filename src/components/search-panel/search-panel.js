517516da61b4249f7f26df87b4a7f84736844af7
Serega Kordeluk <serega.wallace@gmail.com>
2022-01-26 18:03:08 +0300

    search panel

---
+++ src/components/search-panel/search-panel.js
@@ -0,0 +1,12 @@
+import './search-panel.css';
+
+const SearchPanel = () => {
+    return (
+        <input type="text"
+            className="search-input form-control"
+            placeholder="Employee name"
+            />
+    )
+}
+
+export default SearchPanel;
